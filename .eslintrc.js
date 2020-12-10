2e5f256d614a4fe4201885e6a536a2594e5a1da8
peakchen90 <1558267774@qq.com>
2020-12-10 23:42:28 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'alloy',
+    'alloy/typescript'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  globals: {
+    __VERSION__: 'readonly'
+  },
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/member-ordering': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    'no-useless-constructor': 'off',
+    'no-param-reassign': 'off',
+    'quotes': ['error', 'single'],
+    'prefer-template': 'error',
+    'eqeqeq': 'off',
+    'no-eq-null': 'off',
+    'semi': 'error',
+    'max-params': 'off',
+    'no-return-assign': 'off',
+    'max-nested-callbacks': 'off',
+    'no-undef': 'off',
+    'one-var': 'off'
+  }
+};
